ab3b75747c9e2f50c3e9e740041f2146ff95f8b4
srirakshacs <102500631+srirakshacs@users.noreply.github.com>
2023-11-08 12:16:24 +0530

    Created f3.sh

---
+++ f3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "enter the value of n"
+read n
+a=0
+b=1
+count=2
+echo "fibonacci series"
+echo $a
+echo $b
+while [ $count -le $n ]
+do
+fib=`expr $a + $b`
+a=$b
+b=$fib
+echo $fib
+count =`expr $count + 1`
+done
